48bb310d1bf8f7199c87aa106ba2c586a9cd916d
Alexander Smoktal <asmoktal@cybervisiontech.com>
2016-12-15 16:44:29 +0200

    Add artstyle rules

---
+++ astylerc.sh
@@ -0,0 +1,21 @@
+# astyle Config File
+
+recursive
+style=google
+indent=spaces=4
+indent-switches
+indent-preproc-define
+indent-col1-comments
+max-instatement-indent=
+min-conditional-indent=
+break-blocks
+pad-oper
+pad-header
+unpad-paren
+align-pointer=name
+align-reference=name
+add-brackets
+convert-tabs
+close-templates
+max-code-length=120
+break-after-logical
