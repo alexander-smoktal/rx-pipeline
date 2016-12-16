d96c25decc7b46e5249774effd1112fbc8cd9b76
Alexander Smoktal <asmoktal@cybervisiontech.com>
2016-12-16 12:14:55 +0200

    Document file observable

--- src/observables/observable_file.h
+++ src/observables/observable_file.h
@@ -3,4 +3,12 @@
 #include "../loop.h"
 #include "../observable.h"
 
+/**
+ * @brief observable_file_create Create file connector.
+ * @param loop Loop, which runs observable.
+ * @param path Path to file.
+ * @param buffer_size Chunk size for file reads.
+ * @param callback Callback to process file data.
+ * @return New Observable if succesfully, NULL otherwise.
+ */
 Observable *observable_file_create(Loop *loop, const char *path, size_t buffer_size, observable_cb callback);
